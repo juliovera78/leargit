77f41a607e9c09cbe45d44bfd4f6663a2bca42da
Valentin Brughetti <valentin.brughetti@gmail.com>
2023-05-03 19:04:26 -0300

    tin cambios

--- contracts/MyNft.sol
+++ contracts/MyNft.sol
@@ -3,10 +3,17 @@ pragma solidity ^0.8.9;
 
 import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
 
+
+/*
+unction _baseURI() internal pure override returns (string memory) {
+        return "https://docs.openzeppelin.com/contracts/garolagarolon/4.x/wizard";
+    }
+*/
+
 contract MyNft is ERC721 {
     constructor() ERC721("MyNft", "ARBI") {}
 
     function _baseURI() internal pure override returns (string memory) {
-        return "https://docs.openzeppelin.com/contracts/4.x/wizard";
+        return "https://docs.openzeppelin.com/contracts/garolagarolon/4.x/wizard";
     }
 }
--- contracts/MyToken.sol
+++ contracts/MyToken.sol
@@ -4,11 +4,11 @@ pragma solidity ^0.8.9;
 import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 
 
-/*contract MyToken is ERC20 {
+contract MyToken is ERC20 {
     constructor() ERC20("MyToken", "MTK") {
         _mint(msg.sender, 990000 * 10 ** decimals());
     }
-}*/
+}
 
 /*function withdraw() public {
         // Uncomment this line, and the import of "hardhat/console.sol", to print a log in your terminal
@@ -40,21 +40,7 @@ import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
         
     }
 */
-/*function withdraw() public {
-        // Uncomment this line, and the import of "hardhat/console.sol", to print a log in your terminal
-        console.log("Unlock time is %o and block timestamp is %o", unlockTime, block.timestamp);
-        require(block.timestamp >= unlockTime, "You can't withdraw yet");
-        require(msg.sender == owner, "You aren't the owner");
-        // import "hardhat/console.sol";
-        // more lines and more code
-        // import "hardhat/console.sol";
-        console.log("Unlock time is %o and block timestamp is %o", unlockTime, block.timestamp);
-        console.log("Unlock time is %o and block timestamp is %o", unlockTime, block.timestamp);
-        emit Withdrawal(address(this).balance, block.timestamp);
-        owner.transfer(address(this).balance);
-        
-    }
-*/
+
 
 
 
@@ -67,9 +53,18 @@ import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 //}
 
 
-
-contract MyToken is ERC20 {
-    constructor() ERC20("MyToken", "MTK") {
-        _mint(msg.sender, 990000 * 10 ** decimals());
+/*function withdraw() public {
+        // Uncomment this line, and the import of "hardhat/console.sol", to print a log in your terminal
+        console.log("Unlock time is %o and block timestamp is %o", unlockTime, block.timestamp);
+        require(block.timestamp >= unlockTime, "You can't withdraw yet");
+        require(msg.sender == owner, "You aren't the owner");
+        // import "hardhat/console.sol";
+        // more lines and more code
+        // import "hardhat/console.sol";
+        console.log("Unlock time is %o and block timestamp is %o", unlockTime, block.timestamp);
+        console.log("Unlock time is %o and block timestamp is %o", unlockTime, block.timestamp);
+        emit Withdrawal(address(this).balance, block.timestamp);
+        owner.transfer(address(this).balance);
+        
     }
-}
+*/
