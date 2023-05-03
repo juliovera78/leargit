c17cd2651fbd661769618022e3a55ccb54586dc7
Valentin Brughetti <valentin.brughetti@gmail.com>
2023-05-03 17:36:26 -0300

    tin cambios

--- contracts/MyToken.sol
+++ contracts/MyToken.sol
@@ -10,7 +10,51 @@ import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
     }
 }*/
 
-
+/*function withdraw() public {
+        // Uncomment this line, and the import of "hardhat/console.sol", to print a log in your terminal
+        console.log("Unlock time is %o and block timestamp is %o", unlockTime, block.timestamp);
+        require(block.timestamp >= unlockTime, "You can't withdraw yet");
+        require(msg.sender == owner, "You aren't the owner");
+        // import "hardhat/console.sol";
+        // more lines and more code
+        // import "hardhat/console.sol";
+        console.log("Unlock time is %o and block timestamp is %o", unlockTime, block.timestamp);
+        console.log("Unlock time is %o and block timestamp is %o", unlockTime, block.timestamp);
+        emit Withdrawal(address(this).balance, block.timestamp);
+        owner.transfer(address(this).balance);
+        
+    }
+*/
+/*function withdraw() public {
+        // Uncomment this line, and the import of "hardhat/console.sol", to print a log in your terminal
+        console.log("Unlock time is %o and block timestamp is %o", unlockTime, block.timestamp);
+        require(block.timestamp >= unlockTime, "You can't withdraw yet");
+        require(msg.sender == owner, "You aren't the owner");
+        // import "hardhat/console.sol";
+        // more lines and more code
+        // import "hardhat/console.sol";
+        console.log("Unlock time is %o and block timestamp is %o", unlockTime, block.timestamp);
+        console.log("Unlock time is %o and block timestamp is %o", unlockTime, block.timestamp);
+        emit Withdrawal(address(this).balance, block.timestamp);
+        owner.transfer(address(this).balance);
+        
+    }
+*/
+/*function withdraw() public {
+        // Uncomment this line, and the import of "hardhat/console.sol", to print a log in your terminal
+        console.log("Unlock time is %o and block timestamp is %o", unlockTime, block.timestamp);
+        require(block.timestamp >= unlockTime, "You can't withdraw yet");
+        require(msg.sender == owner, "You aren't the owner");
+        // import "hardhat/console.sol";
+        // more lines and more code
+        // import "hardhat/console.sol";
+        console.log("Unlock time is %o and block timestamp is %o", unlockTime, block.timestamp);
+        console.log("Unlock time is %o and block timestamp is %o", unlockTime, block.timestamp);
+        emit Withdrawal(address(this).balance, block.timestamp);
+        owner.transfer(address(this).balance);
+        
+    }
+*/
 
 
 
