d8fdbfa4338d13c0ea198ba7887c69475cd4c500
julio vera <juliovera78@gmail.com>
2023-05-04 15:50:06 -0300

    cambio 10

--- contracts/MyToken.sol
+++ contracts/MyToken.sol
@@ -3,6 +3,12 @@ pragma solidity ^0.8.9;
 
 import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 
+
+
+/* cambio 10
+}*/
+
+
 /*contract MyToken is ERC20 {
     constructor() ERC20("MyToken", "MTK") {
         _mint(msg.sender, 990000 * 10 ** decimals());
